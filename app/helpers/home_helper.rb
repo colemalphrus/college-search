dad888c7f58e941484c21607757ce0d09cc7bd00
colemalphrus <colemalphrus@gmail.com>
2022-10-15 02:50:59 -0400

    helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,30 @@
 module HomeHelper
-  def home_helper_method
 
+  def api_key
+    ENV['API_KEY']
+  end
+
+  def base_url
+    "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=#{api_key}&fields=id,school.name,school.state,location.lat,location.lon&per_page=100"
+  end
+
+  def schools
+    return unless params[:search].present?
+    response = HTTParty.get(
+      base_url + "&school.name=#{params[:search]}",
+      format: :plain
+    )
+    data = JSON.parse response, symbolize_names: true
+    data[:results]
+  end
+
+
+  def map?
+    params[:search].present?
+  end
+
+
+  def test
+    "hola mundo"
   end
 end

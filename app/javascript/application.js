add89c1ac21ac327ec470f51403f391aeb9cd691
colemalphrus <colemalphrus@gmail.com>
2022-10-15 02:48:29 -0400

    maps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,35 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+
+function initMap() {
+    const KC = { lat: 39.1201, lng: -94.5798 };
+    const infoWindow = new google.maps.InfoWindow();
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: KC,
+    });
+    window.map = map
+    window.infoWindow = infoWindow
+}
+
+function addMarkers(schools){
+
+    schools.map((school) => {
+        let marker = new google.maps.Marker({
+            position: { lat: school["location.lat"], lng: school["location.lon"] },
+            title: school["school.name"],
+            map: map,
+        });
+        marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+        });
+    })
+}
+
+
+window.addMarkers = addMarkers
+window.initMap = initMap;
